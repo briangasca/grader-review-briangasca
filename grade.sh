c88b137473d96e7a4d89f0d6a3f1ec8ba7bdfc48
Brian Gasca <bgasca@ucsd.edu>
2024-03-06 14:56:05 -0800

    Fixed grading bugs

--- grade.sh
+++ grade.sh
@@ -14,3 +14,50 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [ -f student-submission/ListExamples.java ]
+then
+    echo "file found"
+else
+    echo "file not found"
+    echo "0%"
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compilation Successful."
+else
+    echo "Compilation Failed."
+    echo "0%"
+    exit
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples | tail -n 2 | head -n 1 > grade.txt
+
+if grep -q 'OK' grade.txt
+then
+    echo "100%"
+    exit
+else
+    grep -Eo '[0-9]+' grade.txt > result.txt
+
+    tests=$(sed -n '1p' "result.txt")
+    failures=$(sed -n '2p' "result.txt")
+
+    tests_mult=$(($tests * 100))
+
+    failures_mult=$(($failures * 100))
+
+    difference=$(($tests_mult - $failures_mult))
+
+    grade=$(($difference / $tests)) 
+    echo $grade"%"
+fi
+
